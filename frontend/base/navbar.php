2d9ea1b2f0538b705801bac6a8e6bb75c785afdb
vagrant <vagrant@homestead>
2020-10-03 18:08:37 +0000

    template changes

---
+++ frontend/base/navbar.php
@@ -0,0 +1,73 @@
+	<body>
+		<header id="header" class="clearfix">
+			<nav class="navbar navbar-default navbar-expand-lg">
+				<div class="container">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#tr-mainmenu" aria-controls="tr-mainmenu" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"><i class="fa fa-align-justify"></i></span>
+					</button>
+					<a class="navbar-brand" href=""><img class="img-fluid" src="./assets/img/logo/logo_112X35.png" alt="Logo"></a>
+					<div class="collapse navbar-collapse" id="tr-mainmenu">
+						<!--<ul class="nav navbar-nav">
+							<li class="active dropdown"><a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown">Home <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li class="active"><a href="https://demo.themeregion.com/trade/index.html">Home Default </a></li>
+								<li><a href="https://demo.themeregion.com/trade/index-one.html">Home Version-1</a></li>
+								<li><a href="https://demo.themeregion.com/trade/index-two.html">Home Version-2</a></li>
+								<li><a href="https://demo.themeregion.com/trade/index-three.html">Home Version-3</a></li>
+								<li><a href="https://demo.themeregion.com/trade/index-car.html">Home Car-1<span class="badge">New</span></a></li>
+								<li><a href="https://demo.themeregion.com/trade/index-car-two.html">Home Car-2<span class="badge">New</span></a></li>
+								<li><a href="https://demo.themeregion.com/trade/directory.html">Home Directory<span class="badge">Latest</span></a></li>
+							</ul>
+						</li>
+						<li><a href="https://demo.themeregion.com/trade/categories.html">Category</a></li>
+						<li><a href="https://demo.themeregion.com/trade/details.html">Ad Details</a></li>
+						<li><a href="https://demo.themeregion.com/trade/faq.html">Help/Support</a></li>
+						<li class="dropdown"><a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown">Pages <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="https://demo.themeregion.com/trade/about-us.html">ABout Us</a></li>
+							<li><a href="https://demo.themeregion.com/trade/contact-us.html">Contact Us</a></li>
+							<li><a href="https://demo.themeregion.com/trade/ad-post.html">Ad post</a></li>
+							<li><a href="https://demo.themeregion.com/trade/ad-post-details.html">Ad post Details</a></li>
+							<li><a href="https://demo.themeregion.com/trade/categories-main.html">Category Ads</a></li>
+							<li><a href="https://demo.themeregion.com/trade/details.html">Ad Details</a></li>
+							<li><a href="https://demo.themeregion.com/trade/my-ads.html">My Ads</a></li>
+							<li><a href="https://demo.themeregion.com/trade/my-profile.html">My Profile</a></li>
+							<li><a href="https://demo.themeregion.com/trade/favourite-ads.html">Favourite Ads</a></li>
+							<li><a href="https://demo.themeregion.com/trade/archived-ads.html">Archived Ads</a></li>
+							<li><a href="https://demo.themeregion.com/trade/pending-ads.html">Pending Ads</a></li>
+							<li><a href="https://demo.themeregion.com/trade/delete-account.html">Close Account</a></li>
+							<li><a href="https://demo.themeregion.com/trade/published.html">Ad Publised</a></li>
+							<li><a href="https://demo.themeregion.com/trade/signup.html">Sign Up</a></li>
+							<li><a href="https://demo.themeregion.com/trade/signin.html">Sign In</a></li>
+							<li><a href="https://demo.themeregion.com/trade/faq.html">FAQ</a></li>
+							<li><a href="https://demo.themeregion.com/trade/coming-soon.html">Coming Soon <span class="badge">New</span></a></li>
+							<li><a href="https://demo.themeregion.com/trade/pricing.html">Pricing<span class="badge">New</span></a></li>
+							<li><a href="https://demo.themeregion.com/trade/500-page.html">500 Opsss<span class="badge">New</span></a></li>
+							<li><a href="https://demo.themeregion.com/trade/404-page.html">404 Error<span class="badge">New</span></a></li>
+						</ul>
+					</li>
+				</ul>-->
+			</div>
+			<div class="nav-right">
+				<div class="dropdown language-dropdown">
+					<i class="fa fa-globe"></i>
+					<a data-toggle="dropdown" href="#"><span class="change-text">United Kingdom</span> <i class="fa fa-angle-down"></i></a>
+					<ul class="dropdown-menu language-change">
+						<li><input type="text" class="form-control"></li>
+						<li><a href="https://demo.themeregion.com/trade/#">United Kingdom</a></li>
+						<li><a href="https://demo.themeregion.com/trade/#">United States</a></li>
+						<li><a href="https://demo.themeregion.com/trade/#">China</a></li>
+						<li><a href="https://demo.themeregion.com/trade/#">Russia</a></li>
+					</ul>
+				</div>
+
+				<ul class="sign-in">
+					<li><i class="fa fa-user"></i></li>
+					<li><a href="signin.php">Sign In</a></li>
+					<li><a href="signup.php">Register</a></li>
+				</ul>
+				<a href="https://demo.themeregion.com/trade/ad-post.html" class="btn">Get Help</a>
+			</div>
+		</div>
+	</nav>
+</header>
